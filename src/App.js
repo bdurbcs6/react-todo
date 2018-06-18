2019cdc5c9ff9ca288e13561aaf9e0dd910fe45d
bdurb <brian@rpmgreenville.com>
2018-06-18 15:30:35 -0700

    removed hard coded state, set default todos to empty array

--- src/App.js
+++ src/App.js
@@ -7,18 +7,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todos: [
-        {
-          task: 'Organize Garage',
-          id: 1528817077286,
-          completed: false
-        },
-        {
-          task: 'Bake Cookies',
-          id: 1528817084358,
-          completed: false
-        }
-      ],
+      todos: [],
       todo: ''
     };
   }
